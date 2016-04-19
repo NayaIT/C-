bb3c5df48c8b3351059652fd65b346dd36b9f70b
NayaIT <n.m.keranova@gmail.com>
2016-04-19 21:55:54 +0300

    Update of QuadraticEquation/Equation.cs file

--- ConsoleInAndOut/QuadraticEquation/Equation.cs
+++ ConsoleInAndOut/QuadraticEquation/Equation.cs
@@ -23,81 +23,44 @@ namespace QuadraticEquation
     {
         static void Main()
         {
-            //Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
             double coefficientA = double.Parse(Console.ReadLine());
             double coefficientB = double.Parse(Console.ReadLine());
             double coefficientC = double.Parse(Console.ReadLine());
-            
-            //string output = string.Empty;
 
-            //double discriminant = Math.Pow(coefficientB, 2) - 4 * coefficientA * coefficientC; 
-            double discriminant = (coefficientB * coefficientB) - (4 * coefficientA * coefficientC);
-            double discriminantRoot = Math.Sqrt(discriminant);
 
-            if (discriminant < 0)
+            double x1, x2;
+
+            //Discriminant < 0
+            if (coefficientB * coefficientB - 4 * coefficientA * coefficientC < 0)                                              
             {
                 Console.WriteLine("no real roots");
             }
-            else if (discriminant == 0)
-            {
-
-                double x = -coefficientB / (2 * coefficientA); //-b / (2 * a);
-
-                Console.WriteLine("{0:0.00}", x);
-            }
             else
             {
-                
-                double x1 = (-coefficientB + discriminantRoot) / (2 * coefficientA);
-                double x2 = (-coefficientB - discriminantRoot) / (2 * coefficientA);
+                //Discriminant == 0    
+                if (coefficientB * coefficientB - 4 * coefficientA * coefficientC == 0)                                    
+                {
+                    x1 = x2 = -coefficientB / (2 * coefficientA);
+                    Console.WriteLine("{0:F2}", x1);
+                }
+                //Discriminant > 0
+                else
+                {
+                    x1 = (-coefficientB - Math.Sqrt(coefficientB * coefficientB - 4 * coefficientA * coefficientC)) / (2 * coefficientA);
+                    x2 = (-coefficientB + Math.Sqrt(coefficientB * coefficientB - 4 * coefficientA * coefficientC)) / (2 * coefficientA);
 
-                double maxX = Math.Max(x1, x2);
-                double minX = Math.Min(x1, x2);
-
-                Console.WriteLine("{0:0.00} \n{1:0.00}", maxX, minX);
-
-                //if (x1 > x2)
-                //{
-                //    Console.WriteLine("{0:0.00} \n{1:0.00}", x1, x2);
-                //}
-                //else
-                //{
-                //    Console.WriteLine("{0:0.00} \n{1:0.00}", x2, x1);
-                //}
+                    if (x1 < x2)
+                    {
+                        Console.WriteLine("{0:F2}\r\n{1:F2}", x1, x2);
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0:F2}\r\n{1:F2}", x2, x1);
+                    }
+                }
             }
-          
+
         }
     }
 }
-
-/*
-        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
-        double a = double.Parse(Console.ReadLine());
-        double b = double.Parse(Console.ReadLine());
-        double c = double.Parse(Console.ReadLine());
-
-        string output = string.Empty;
-
-        double discriminant = (b * b) - (4 * a * c);
-        if (discriminant < 0)
-        {
-            output = "no real roots";
-        }
-        else if (discriminant == 0)
-        {
-            double root = -b / (2 * a);
-            output = string.Format("{0:F2}", root);
-        }
-        else
-        {
-            double firstRoot = (-b + Math.Sqrt(discriminant)) / (2 * a);
-            double secondRoot = (-b - Math.Sqrt(discriminant)) / (2 * a);
-            string formatedOutput = "{0:F2}\n{1:F2}";
-
-            output = firstRoot < secondRoot ?
-               string.Format(formatedOutput, firstRoot, secondRoot) :
-               string.Format(formatedOutput, secondRoot, firstRoot);
-        }
-
-        Console.WriteLine(output);
-*/
